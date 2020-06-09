564d0cc833602221f6692ac227a059f8d50d3925
Chris-SG <christopher.shandgost@myob.com>
2020-06-09 11:34:39 +1000

    Text change

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PageContainer from '../containers/PageContainer';
 
-ReactDOM.render(<PageContainer render={() => 'Hello'} />, document.getElementById('root'));
+ReactDOM.render(<PageContainer
+  render={() => 'I have migrated the tenant for this website. This means that you will '
+    + 'be logged out and your credentials will have expired. Anyone who has registered '
+    + 'will need to reset their password by clicking \'Forgot Password\', while anyone '
+    + 'who logged in with Google will just need to connect again.'}
+/>, document.getElementById('root'));
