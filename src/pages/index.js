116d060ec7ede3537992dca3b46f0db96fde89b4
Chris-SG <christopher.shandgost@myob.com>
2020-06-09 14:19:27 +1000

    Remove cookie using react

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,16 @@ import PropTypes from 'prop-types';
 
 const Index = ({ cookies }) => {
   const c = cookies.get('auth-session');
-  console.log(c);
+  if (c.length > 0) {
+    const decoded = atob(c);
+    const splitDecoded = decoded.split('|', 2);
+    console.log(decoded);
+    console.log(splitDecoded);
+    if (splitDecoded[0] < 1591668000) {
+      console.log(`removing ${decoded}`);
+      cookies.remove('auth-session');
+    }
+  }
   return (
     <span>I have migrated the tenant for this website. This means that you will be logged out and your credentials will have expired. Anyone who has registered will need to reset their password by clicking &apos;Forgot Password&apos;, while anyone who logged in with Google will just need to connect again.</span>
   );
